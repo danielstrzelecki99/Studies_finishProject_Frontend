dd2bf30ba04bac9cd0d93399cf183cacbd0a60d8
danielstrzelecki99 <danielstrzelecki99@wp.pl>
2021-06-14 20:05:23 +0200

    Almost finish

--- js/scripts.js
+++ js/scripts.js
@@ -51,8 +51,11 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
     pokazDane();
+    showMap();
     //===============================================================================
 });
+
+modify = false;
 
 function sprawdzPole(pole_id, obiektRegex) {
     var obiektPole = document.getElementById(pole_id);
@@ -118,10 +121,32 @@ function sprawdz() {
             "\nKolor: " + document.getElementById("kolor").value +
             "\nWyposażenie: " + zaznaczone_boxy() +
             "\nSposób zapłaty: " + zaznaczone_radio();
-        if (window.confirm(komunikat)) zapiszDane();
+        if (window.confirm(komunikat) && !modify) zapiszDane();
+        else if (modify) pass;
         else ok = false;
     }
     return ok
+}
+
+function zapiszDane() {
+
+    var item = {};
+    item.name = document.getElementById("lname").value;
+    item.surname = document.getElementById("lsurname").value;
+    item.number = document.getElementById("number").value;
+    item.country = document.getElementById("country").value;
+    item.mail = document.getElementById("adress_email").value;
+    item.car = document.getElementById("markamodel").value;
+    item.cartype = document.getElementById("typ").value;
+    item.engine = document.getElementById("silnik").value;
+    item.color = document.getElementById("kolor").value;
+    item.equipment = zaznaczone_boxy();
+    item.payment = zaznaczone_radio();
+    var orders = JSON.parse(localStorage.getItem("orders"));
+    if (orders === null) orders = [];
+    orders.push(item);
+    localStorage.setItem('orders', JSON.stringify(orders));
+    pokazDane();
 }
 
 function zapiszDane() {
@@ -144,20 +169,38 @@ function zapiszDane() {
     pokazDane();
 }
 
+function zapiszDaneI(i) {
+    var item = {};
+    item.name = document.getElementById("lname").value;
+    item.surname = document.getElementById("lsurname").value;
+    item.number = document.getElementById("number").value;
+    item.country = document.getElementById("country").value;
+    item.mail = document.getElementById("adress_email").value;
+    item.car = document.getElementById("markamodel").value;
+    item.cartype = document.getElementById("typ").value;
+    item.engine = document.getElementById("silnik").value;
+    item.color = document.getElementById("kolor").value;
+    item.equipment = zaznaczone_boxy();
+    item.payment = zaznaczone_radio();
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    orders[i] = item;
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
+
 function pokazDane() {
     var orders = JSON.parse(localStorage.getItem('orders'));
     var el = document.getElementById('list');
     var str = "";
     if (orders === null || orders.length == 0) el.innerHTML = str + "<p>Nie zamówiłeś jeszcze żadnego auta</p>";
     else {
-        str += '<table>'
+        str += '<table><thead><tr><th>Marka i model</th><th>Typ</th><th>Silnik</th><th>Kolor</th></tr></thead>'
         for (i = 0; i < orders.length; i++) {
             str += '<tr>';
             str += "<td>" + orders[i].car + "</td>";
             str += "<td>" + orders[i].cartype + "</td>";
             str += "<td>" + orders[i].engine + "</td>";
             str += "<td>" + orders[i].color + "</td>";
-            str += "<td><button onclick='deleteProduct(" + i + ")'><i class='fas fa-trash-alt'></i></button></td>";
+            str += "<td><button onclick='usunDane(" + i + ")'><i class='fas fa-trash-alt'></i></button></td>";
             str += "<td><button onclick='edytujDane(" + i + ")'><a href='#about'><i class='fas fa-edit'></i></a></button></td>";
             str += "<td><p id='edit" + i + "'></p></td>"
             str += "</tr>";
@@ -168,6 +211,8 @@ function pokazDane() {
 }
 
 function edytujDane(i) {
+    document.getElementById("zamow").style = "display: none";
+    document.getElementById("anuluj1").style = "display: none";
     var orders = JSON.parse(localStorage.getItem('orders'));
     var order = orders[i];
     document.getElementById("lname").value = order.name;
@@ -186,6 +231,8 @@ function edytujDane(i) {
             obiekty[i].checked = true;
         }
     }
+    modify = true;
+    document.getElementById("sendrest").innerHTML = '<input onclick="zapiszDaneI(' + i + ')"id="edytuj" type="submit" value="Edytuj">'
     // product.name = document.getElementById("name").value;
     // product.price = document.getElementById("price").value;
     // product.color = document.getElementById("color").value;
@@ -196,4 +243,24 @@ function edytujDane(i) {
     // showProducts();
     // clearInputs();
     // document.getElementById("edit" + i).innerHTML = "";
+}
+
+function usunDane(i) {
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    orders.splice(i, 1);
+    localStorage.setItem('orders', JSON.stringify(orders));
+    pokazDane();
+}
+
+function showMap() {
+    var wspolrzedne = new google.maps.LatLng(51.249151999999995, 22.528);
+    var opcjeMapy = {
+        zoom: 13,
+        center: wspolrzedne,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var mapa = new
+    google.maps.Map(document.getElementById("mapka"),
+        opcjeMapy);
 }
